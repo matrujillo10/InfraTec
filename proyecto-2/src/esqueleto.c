29ad1ac87fe776caef34adb27003ef2775f4ce57
miller-trujillo <ma.trujillo10@uniandes.edu.co>
2016-11-22 13:02:05 -0500

    Corrección etiquetas y porcs

--- proyecto-2/src/esqueleto.c
+++ proyecto-2/src/esqueleto.c
@@ -92,36 +92,33 @@ int main(int argc, char* argv[])
  */
 int convertir (char * p) {
     __asm {
-        .code
-            convertir proc
-                push ebp
-                mov ebp, esp
-                mov ecx, [ebp+8]                ; ecx guarda el parametro
-                mov ebx, 45                     ; ebx se igual al valor númerico del caracter ascii '-'
-                cmp ebx, ecx                    ; se compara con el valor almacenado en ecx
-                jne notEquals
-                    mov esi, 0
-                    jmp end
-                notEquals:
-                mov ebx, 97
-                add ecx, 1
-                cmp ebx, ecx
-                jne notEqualsA
-                    mov esi, 1
-                    jmp end
-                notEqualsA:
-                mov ebx, 98
-                cmp ebx, ecx
-                jne notEqualsB
-                    mov esi, 2
-                    jmp end
-                notEqualsB:
-                mov esi, 0
-                end:
-                mov eax, esi
-                pop ebp
-                ret 4
-            convertir endp
+        push ebp
+        mov ebp, esp
+        mov ecx, [ebp+8]                ; ecx guarda el parametro
+        mov ebx, 45                     ; ebx se igual al valor númerico del caracter ascii '-'
+        cmp ebx, ecx                    ; se compara con el valor almacenado en ecx
+        jne notEquals
+            mov esi, 0
+            jmp end
+        notEquals:
+        mov ebx, 97
+        add ecx, 1
+        cmp ebx, ecx
+        jne notEqualsA
+            mov esi, 1
+            jmp end
+        notEqualsA:
+        mov ebx, 98
+        cmp ebx, ecx
+        jne notEqualsB
+            mov esi, 2
+            jmp end
+        notEqualsB:
+        mov esi, 0
+        end:
+        mov eax, esi
+        pop ebp
+        ret 4
     }
 }
 
@@ -132,114 +129,107 @@ int convertir (char * p) {
 void conversionTexto(ARCHIVO *arch, ARCHIVO *resultado) {
     __asm {
         ; Se usan los registros ebx, ecx, esi y edi. El registro eax se usa para guardar los valores retornados por los métodos.
-        .code
-            conversionTexto proc
-                push ebp
-                mov ebp, esp
-                mov ebx, [ebp+8]                ; ebx guarda el inicio de la estructura arch. Es decir, el tamaño.
-                mov edx, [ebp+12]               ; edx guarda el inicio de la estructura resultado. Es decir, el tamaño.
-                mov ecx, [edx+4]
-                mov esi, 0
-                Bwhile:	cmp esi, [edx]
-                jge Ewhile                      ; i < resultado->tamanho
-                    
-                    push ebx                    ;----------------------------------------------------------------------
-                    push edx                    ;
-                    push ecx                    ; Salvando los registros
-                    push esi                    ;----------------------------------------------------------------------
-                        
-                    push esi                    ; Parametros  sacar5bits - i
-                    push [ebx]                  ; Parametros  sacar5bits - arch
-                    call sacar5bits             ; eax se usa para traer el valor retornado
-                    push eax                    ; Parametros codificar
-                        
-                    call codificar              ; eax se usa para traer el valor retornado
-                        
-                    pop esi                     ;----------------------------------------------------------------------
-                    pop ecx                     ;
-                    pop edx                     ; Recuperando los registros
-                    pop ebx                     ;----------------------------------------------------------------------
-                        
-                    mov edi, [edx+4]
-                    add edi, esi
-                    mov [edi], eax
-                    inc esi
-                jmp Bwhile
-                Ewhile:
-                pop ebp
-                ret 8
-            conversionTexto endp
+        push ebp
+        mov ebp, esp
+        mov ebx, [ebp+8]                ; ebx guarda el inicio de la estructura arch. Es decir, el tamaño.
+        mov edx, [ebp+12]               ; edx guarda el inicio de la estructura resultado. Es decir, el tamaño.
+        mov ecx, [edx+4]
+        mov esi, 0
+        Bwhile:	cmp esi, [edx]
+        jge Ewhile                      ; i < resultado->tamanho
+
+            push ebx                    ;----------------------------------------------------------------------
+            push edx                    ;
+            push ecx                    ; Salvando los registros
+            push esi                    ;----------------------------------------------------------------------
+
+            push esi                    ; Parametros  sacar5bits - i
+            push [ebx]                  ; Parametros  sacar5bits - arch
+            call sacar5bits             ; eax se usa para traer el valor retornado
+            push eax                    ; Parametros codificar
+
+            call codificar              ; eax se usa para traer el valor retornado
+
+            pop esi                     ;----------------------------------------------------------------------
+            pop ecx                     ;
+            pop edx                     ; Recuperando los registros
+            pop ebx                     ;----------------------------------------------------------------------
+
+            mov edi, [edx+4]
+            add edi, esi
+            mov [edi], eax
+            inc esi
+        jmp Bwhile
+        Ewhile:
+        pop ebp
+        ret 8
     }
 }
 
-/*
-* Procedimiento que extrae el n-ésimo grupo de 5 bits de la entrada (estructura ARCHIVO apuntada por 'arch') y retorna un char que los contiene en sus 5 bits menos significativos
-* (los restantes bits deben estar en cero)
+/**
+* Procedimiento que extrae el n-ésimo grupo de 5 bits de la entrada (estructura ARCHIVO apuntada por 'arch') y 
+* retorna un char que los contiene * en sus 5 bits menos significativos (los restantes bits deben estar en cero)
 */
 unsigned char sacar5bits(ARCHIVO *arch, int n) {
     __asm {
-        .data 
-            numL DD 0
-            numB DD 0
-        .code
-            sacar5bits proc
-                push ebp
-                mov ebp, esp
-                mov ebx, [ebp+8]                ; arch
-                mov ecx, [ebp+12]               ; n
-                mov eax, [ebp+12]
-                imul 5
-                idiv 8
-                mov numL, eax
-                mov eax, [ebp+12]
-                imul 5
-                idiv 8
-                mov numB, edx
-                mov edx, [ebx+4]
-                add edx, numL
-                mov eax, [edx]
-                mov edx, eax
-                cmp numB, 4
-                jge Else
-                    mov eax, 3
+        numL DD 0
+        numB DD 0
+        push ebp
+        mov ebp, esp
+        mov ebx, [ebp+8]                ; arch
+        mov ecx, [ebp+12]               ; n
+        mov eax, [ebp+12]
+        imul 5
+        idiv 8
+        mov numL, eax
+        mov eax, [ebp+12]
+        imul 5
+        idiv 8
+        mov numB, edx
+        mov edx, [ebx+4]
+        add edx, numL
+        mov eax, [edx]
+        mov edx, eax
+        cmp numB, 4
+        jge Else
+            mov eax, 3
+            sub eax, numB
+            sar edx, eax
+            mov eax, edx
+            and eax, 0x1F ;31
+            jmp fin
+        Else1:
+            mov eax, [ebx]
+            dec eax
+            cmp eax, numL
+            jne Else2
+                    mov eax, 7
                     sub eax, numB
-                    sar edx, eax
+                    mov edi, 4
+                    sub edi, eax
                     mov eax, edx
+                    sal eax, edi
                     and eax, 0x1F ;31
-                    jmp fin
-                Else1:
-                    mov eax, [ebx]
-                    dec eax
-                    cmp eax, numL
-                    jne Else2
-                            mov eax, 7
-                            sub eax, numB
-                            mov edi, 4
-                            sub edi, eax
-                            mov eax, edx
-                            sal eax, edi
-                            and eax, 0x1F ;31
-                        jmp fin
-                    Else2:
-                            mov eax, 7
-                            sub eax, numB
-                            mov edi, 4
-                            sub edi, eax
-                            mov eax, edx
-                            sal eax, edi
-                            and eax, 0x1F ;31
-                            inc numL
-                            mov edi, [ebx+4]
-                            add edi, numL
-                            mov edx, [edi]
-                            mov edi, 11
-                            sub edi, numB
-                            sar edx, edi
-                            or eax, edx
-                fin:
-                pop ebp
-                ret 8
-            sacar5bits endp
+                jmp fin
+            Else2:
+                    mov eax, 7
+                    sub eax, numB
+                    mov edi, 4
+                    sub edi, eax
+                    mov eax, edx
+                    sal eax, edi
+                    and eax, 0x1F ;31
+                    inc numL
+                    mov edi, [ebx+4]
+                    add edi, numL
+                    mov edx, [edi]
+                    mov edi, 11
+                    sub edi, numB
+                    sar edx, edi
+                    or eax, edx
+        fin:
+        pop ebp
+        ret 8
     }
 }
 
@@ -254,26 +244,23 @@ void meter5bits(ARCHIVO *arch, int n, unsigned char bits) {
 /*
 * Procedimiento que recibe un byte con informaci�n en los 5 bits menos significativos, y realiza la codificaci�n correspondiente en ASCII
 */
-//TODO: DESARROLLAR COMPLETAMENTE ESTA FUNCION. SE PERMITE USAR NOMBRES SIMBOLICOS
 unsigned char codificar(unsigned char cinco) {
     __asm {
-        .code
-            codificar proc
-                push ebp
-                mov ebp, esp
-                mov ebx, [esp+8]
-                cmp ebx, 0
-                jl Else
-                    cmp ebx, 0x0f ;15
-                    jg Else
-                        mov eax, 0x40 ;64
-                        or eax, ebx
-                        jmp fin
-                Else:
-                    mov eax, 0x20 ;32
-                    or eax, ebx
-                fin:
-            codificar endp
+        push ebp
+        mov ebp, esp
+        mov ebx, [esp+8]
+        cmp ebx, 0
+        jl Else
+            cmp ebx, 0x0f ;15
+            jg Else
+                mov eax, 0x40 ;64
+                or eax, ebx
+                jmp fin
+        Else:
+            mov eax, 0x20 ;32
+            or eax, ebx
+        fin:
+        ret 4
     }
 }
 
@@ -281,39 +268,35 @@ unsigned char codificar(unsigned char cinco) {
 * Procedimiento que convierte la informaci�n en ASCII de la estructura ARCHIVO apuntada por 'data' (archivo de texto) * y la guarda en la estructura ARCHIVO apuntada por 'resultado'.
 * como una serie de bytes en la que a cada caracter se le extraen sus �ltimos 5 bits y se guardan en la salida unos a continuaci�n de los otros
 */
-//TODO: DESARROLLAR COMPLETAMENTE ESTA FUNCION. SE PERMITE USAR NOMBRES SIMBOLICOS
 void conversionBinario(ARCHIVO *data, ARCHIVO *resultado) {
     __asm {
-        .code
-            conversionBinario proc
-                push ebp
-                mov ebp, esp
-                mov ebx, [ebp+8]                ; data
-                mov ecx, [ebp+12]               ; resultado
-                mov esi, 0                      ; i
-                Bwhile: cmp esi, [ebx]
-                jge Ewhile
-                    mov edi, [ebx+4]
-                    add edi, esi
-                    mov edx, [edi]
-        
-                    push ebx
-                    push ecx
-                    push esi
-                        
-                    push edx
-                    push esi
-                    push ecx
-                    call meter5bits
-                    
-                    pop esi
-                    pop ecx
-                    pop ebx
-                    inc esi
-                jmp Bwhile
-                Ewhile:
-                ret 8
-            conversionBinario endp
+        push ebp
+        mov ebp, esp
+        mov ebx, [ebp+8]                ; data
+        mov ecx, [ebp+12]               ; resultado
+        mov esi, 0                      ; i
+        Bwhile: cmp esi, [ebx]
+        jge Ewhile
+            mov edi, [ebx+4]
+            add edi, esi
+            mov edx, [edi]
+
+            push ebx
+            push ecx
+            push esi
+
+            push edx
+            push esi
+            push ecx
+            call meter5bits
+
+            pop esi
+            pop ecx
+            pop ebx
+            inc esi
+        jmp Bwhile
+        Ewhile:
+        ret 8
     }
 }
 
